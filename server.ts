2a9b018c6a5e68ace9f44e9de41fe4bfcc2fd92f
Birendra Bhandari <152133675+birendrabhandari@users.noreply.github.com>
2026-02-28 15:19:29 +0530

    server.ts

--- server.ts
+++ server.ts
@@ -142,7 +142,8 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    app.use(express.static(path.join(__dirname, "dist")));
+    app.use(express.static(__dirname));
+    
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, 'index.html'));
   
