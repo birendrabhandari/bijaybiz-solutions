61f5d6bc9b7272d7ea38513a1405ec66aecd9f11
Birendra Bhandari <152133675+birendrabhandari@users.noreply.github.com>
2026-02-28 15:11:19 +0530

    server.ts

--- server.ts
+++ server.ts
@@ -144,7 +144,8 @@ async function startServer() {
   } else {
     app.use(express.static(path.join(__dirname, "dist")));
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "dist", "index.html"));
+      res.sendFile(path.join(__dirname, 'index.html'));
+  
     });
   }
 
